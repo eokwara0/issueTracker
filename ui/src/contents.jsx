b708b3774f7f4651825b4ddb166064f9cdcb6bfe
emmanuel <emmanuelokwara001@gmail.com>
2022-12-20 11:53:32 +0200

    adding_Issue_editing

--- ui/src/contents.jsx
+++ ui/src/contents.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { Switch , Route , Redirect } from 'react-router-dom';
 import IssueList from './IssueList.jsx';
 import IssueReport from './IssueReport.jsx';
+import IssueEdit from './IssueEdit.jsx';
 
 
-const NotFound = () => <h1> Page Not Found</h1>;
+const NotFound = () => <h1>👻🙅🏽‍♀️⚔️🤖🚀🚀🐫🐔 Not Found!! </h1>;
 
 export default function Contents() {
     return (
@@ -12,6 +13,8 @@ export default function Contents() {
             <Redirect exact from="/" to="/issues"/>
             <Route path="/issues" component={IssueList} />
             <Route path="/report" component={IssueReport} />
+            <Route path="/edit/:id" component={IssueEdit} />
+            <Route component={NotFound}/>
         </Switch>
     );
 };
