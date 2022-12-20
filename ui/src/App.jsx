b5164e8e98e19fed7648d6b68aa66479b7726514
emmanuel <emmanuelokwara001@gmail.com>
2022-12-20 08:27:01 +0200

    chore:adding_client_side_routing_and_nav_feature

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -4,10 +4,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'babel-polyfill';
 import 'whatwg-fetch';
-import IssueList from './IssueList.jsx';
+import { HashRouter as Router  } from 'react-router-dom';
+import Page from './Page.jsx';
 
-
-const element = <IssueList/>;
+const element = (
+    <Router>
+        <Page/>
+    </Router>
+)
 const root = ReactDOM.createRoot( document.getElementById('content') )
 root.render( element )
 if(module.hot){
