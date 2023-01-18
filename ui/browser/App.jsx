a36f7843c93707f989ab8a873fb30cb644b53d2e
emmanuel <emmanuelokwara001@gmail.com>
2023-01-18 09:47:45 +0200

    enhancement:create server-rendering for About.jsx - App.jsx -> Added a feature - fix: dockerfile -> changed the start command - chore: About.js -> Babel transformed version of About.jsx - chore: render.js -> returns back a method that returns a template from   template.js - chore: template.js -> creates a template from body parameter - chore: contents.jsx -> Added about route to the router - chore: uiserver.js -> Added endpoint for the serving of About page

--- ui/browser/App.jsx
+++ ui/browser/App.jsx
@@ -1,5 +1,4 @@
 
-
 import React from 'react';
 import * as ReactDOM from 'react-dom/client'
 import 'babel-polyfill';
--- ui/src/Page.jsx
+++ ui/src/Page.jsx
@@ -9,29 +9,34 @@ import IssueAddNavItem from "./IssueAddNavItem.jsx";
 
 function NavBar(){
     return (
-        <Navbar>
-            <Navbar.Header>
-                <Navbar.Brand>Issue Tracker</Navbar.Brand>
-            </Navbar.Header>
-            <Nav pullRight={false}>
-                <Stack direction="horizontal"> 
-                    <Navbar.Link href="/">Home</Navbar.Link>
-                    <Navbar.Link href="/issues">Issue List</Navbar.Link>
-                    <Navbar.Link href="/report">Report</Navbar.Link>
-                </Stack>
-            </Nav>
-            <Nav pullRight>
-                {/* <NavItem>
+      <Navbar>
+        <Navbar.Header>
+          <Navbar.Brand>Issue Tracker</Navbar.Brand>
+        </Navbar.Header>
+        <Nav pullRight={false}>
+          <Stack direction="horizontal">
+            <Navbar.Link href="/">Home</Navbar.Link>
+            <Navbar.Link href="/issues">Issue List</Navbar.Link>
+            <Navbar.Link href="/report">Report</Navbar.Link>
+          </Stack>
+        </Nav>
+        <Nav pullRight>
+          {/* <NavItem>
                     <OverlayTrigger placement="bottom" delayShow={1000} overlay={ <Tooltip id="create-issue">Create Issue</Tooltip>}><Glyphicon glyph="plus"/></OverlayTrigger>
                 </NavItem> */}
-                <IssueAddNavItem/>
-                <NavDropdown id="user-dropdown" title={<Glyphicon glyph="option-vertical" />} noCaret>
-                    <MenuItem>About</MenuItem>
-                </NavDropdown>
-            </Nav>
-        </Navbar>
-        
-    )
+          <IssueAddNavItem />
+          <NavDropdown
+            id="user-dropdown"
+            title={<Glyphicon glyph="option-vertical" />}
+            noCaret
+          >
+            <Navbar.Link href="/about">
+              <MenuItem>About</MenuItem>
+            </Navbar.Link>
+          </NavDropdown>
+        </Nav>
+      </Navbar>
+    );
 }
 
 function Footer() {
