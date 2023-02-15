3163cbeccd2374c7142024936079ca3ae757f6d6
eokwara <emmanuelokwara001@gmail.com>
2023-02-15 15:07:17 +0200

    chore:comments

--- ui/server/render.jsx
+++ ui/server/render.jsx
@@ -3,9 +3,24 @@ import ReactDOMServer from 'react-dom/server';
 import About from '../src/About.jsx';
 import template from './template.js';
 
+
+/**
+ * renders the About component on the server
+ * @param {request} request
+ * @param {response} response
+ */
 function render(req,res ) {
+
+    /**  converting About component into String */
     const body = ReactDOMServer.renderToString(<About/>);
+
+    /** 
+     * sends the string 
+     * representation whenever a user requests for the page 
+     * */
     res.send(template(body));
+
 }
 
+// return render function
 export default render;
